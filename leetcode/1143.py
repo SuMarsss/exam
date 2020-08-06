f00f4c7d81dcc3507368116abc1e39c49c06935d
Mat_Labtop <1204568237@qq.com>
2020-08-06 21:29:21 +0800

    modify a few lines

--- leetcode/1143.py
+++ leetcode/1143.py
@@ -1,10 +1,16 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = [[0] * (N+1) for i in range(N+1)]
-        for i in range(N):
-            for j in range(N):
-                if seq1[i] == seq2[j]:
-                    dp[i+1][j+1] = dp[i][j] + 1
+        m, n = len(text1), len(text2)
+        # 构建 DP table 和 base case
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        # 进行状态转移
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    # 找到一个 lcs 中的字符
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
                 else:
-                    dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
         return dp[-1][-1]
+
